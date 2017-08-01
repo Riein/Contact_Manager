00d5c40e01b183ab289bf6b4993213aac5dd8eca
Sasha Demyanik <ttsvan@msn.com>
2017-07-31 20:12:20 -0700

    Adds AppStore class information to handle actions.

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -5,3 +5,31 @@ import {EventEmitter} from 'events';
 const CHANGE_EVENT = 'change';
 
 let _contacts = [];
+
+class AppStoreClass extends EventEmitter {
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  addChangeListener(callback){
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback){
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  getContacts() {
+    return _contacts;
+  }
+}
+
+const AppStore = new AppStoreClass();
+
+AppStore.dispatchToken = AppDispatcher.register(action => {
+  switch(action.actionType) {
+    
+  }
+});
+
+export default AppStore;
