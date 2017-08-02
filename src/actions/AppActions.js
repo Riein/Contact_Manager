23950b5460419fdfa23d67be10a572e860aaa723
Sasha Demyanik <ttsvan@msn.com>
2017-08-01 21:20:31 -0700

    Adds more dispatcher code to AppActions and AppStore.

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,5 +1,23 @@
 import AppDispatcher from '../dispatcher/AppDispatcher';
+import AppConstants from '../constants/AppConstants';
+import ContactsAPI from '../utils/ContactsAPI';
 
 export default {
-  
+  receiveContacts: () => {
+    ContactsAPI
+    .getContacts('https://jsonplaceholder.typicode.com/users')
+    .then(contacts => {
+      AppDispatcher.dispatch({
+        actionType: AppConstants.RECIEVE_CONTACTS,
+        contacts: contacts
+      });
+    })
+    .catch(message => {
+      AppDispatcher.dispatch({
+        actionType: AppConstants.RECIEVE_CONTACTS_ERROR,
+        message: message
+      });
+    });
+
+  }
 }
